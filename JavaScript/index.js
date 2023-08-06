702e284c17bcc4f7ea254faba7a5c6aee6d9d8b3
srajan-kiyotaka <srajanstark.ash@gmail.com>
2023-08-06 15:16:19 +0530

    Done making the card to display the Project in the Project Section.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#element', {
-    strings: ['AI/ML,', 'Deep Learning,', 'Reinforcement Learning,', 'and Computer Vision!'],
+    strings: ['AI/ML', 'Deep Learning', 'Reinforcement Learning', 'Computer Vision', 'Open Source Contribution!'],
     typeSpeed: 60,
     loop: true,
     cursorChar: '_',
